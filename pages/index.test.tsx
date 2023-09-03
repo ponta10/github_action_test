703f90c68bb8144ade04d48bb992467d0c3ec0c6
ponta10 <mponta2002@gmail.com>
2023-09-03 20:53:02 +0900

    add test

---
+++ pages/index.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import Home from './index';
+
+describe('Home', () => {
+  it('renders without crashing', () => {
+    const { getByText } = render(<Home />);
+    expect(getByText('Get started by editing')).toBeTruthy();
+  });
+});
